27ca22f43e62150f7081807248e5ef92b83be02f
Sachin Singh <sachin.innoventes@phonepe.com>
2020-06-27 14:00:46 +0530

    first commit

---
+++ src/views/todo-view.js
@@ -0,0 +1,12 @@
+import { LitElement, html } from '@polymer/lit-element';
+
+class TodoView extends LitElement {
+
+    render() {
+        return html`
+            <p>Hello World</p>
+        `;
+    }
+}
+
+customElements.define('todo-view', TodoView);
